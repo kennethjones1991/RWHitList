dee4547621cc1624165706d7186b27e515fd56b1
Kenneth Jones <kennethjones1991@outlook.com>
2020-11-24 01:11:20 -0700

    Added functionality to load saved items; completed guided project

--- HitList/ViewController.swift
+++ HitList/ViewController.swift
@@ -20,6 +20,22 @@ class ViewController: UIViewController {
         title = "The List"
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Person")
+        
+        do {
+            people = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
 
     @IBAction func addName(_ sender: UIBarButtonItem) {
         let alert = UIAlertController(title: "New Name", message: "Add a new name", preferredStyle: .alert)
