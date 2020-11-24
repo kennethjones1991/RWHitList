0062327d9947db3314cff41701f3e5a22720d020
Kenneth Jones <kennethjones1991@outlook.com>
2020-11-24 00:33:31 -0700

    Created initial project

---
+++ HitList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HitList
+//
+//  Created by Kenneth Jones on 11/24/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
